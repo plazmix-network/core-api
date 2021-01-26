d7f2b86866fa2a8b5c458b0aa1d040adaa143e23
MasterCapeXD <mastercapexd@gmail.com>
2021-01-26 13:34:19 +0200

    Update configuration api

--- src/main/java/net/plazmix/core/api/common/config/AbstractConfig.java
+++ src/main/java/net/plazmix/core/api/common/config/AbstractConfig.java
@@ -1,2 +1,54 @@
-package net.plazmix.core.api.common.config;public class AbstractConfig {
+package net.plazmix.core.api.common.config;
+
+import com.google.common.base.Preconditions;
+import net.plazmix.core.api.Core;
+
+import java.io.File;
+import java.io.IOException;
+
+public abstract class AbstractConfig<T> implements Config<T> {
+
+    protected final File file;
+    protected T config;
+
+    protected AbstractConfig(File folder, String fileName) {
+        this.file = new File(folder + File.separator + fileName);
+    }
+
+    protected AbstractConfig(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public String getName() {
+        return file.getName();
+    }
+
+    @Override
+    public T unwrap() {
+        Preconditions.checkState(config != null, "Configuration is not loaded!");
+        return config;
+    }
+
+    @Override
+    public boolean exists() {
+        return file.exists();
+    }
+
+    @Override
+    public Config<T> create() {
+        if (!exists())
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                Core.getApi().getLogger().warning("Unable to create this file! Details: " + e.getMessage());
+            }
+        return this;
+    }
+
+    @Override
+    public void delete() {
+        if (exists())
+            file.delete();
+    }
 }
--- src/main/java/net/plazmix/core/api/common/config/Config.java
+++ src/main/java/net/plazmix/core/api/common/config/Config.java
@@ -1,2 +1,45 @@
-package net.plazmix.core.api.common.config;public interface Config {
+package net.plazmix.core.api.common.config;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface Config<T> {
+
+    String getName();
+
+    T unwrap();
+
+    boolean exists();
+
+    Config<T> create();
+
+    Config<T> load();
+
+    Config<T> save();
+
+    void delete();
+
+    Object getObject(String path);
+
+    Object getObject(String path, Object def);
+
+    List<String> getStringList(String path);
+
+    String getString(String path);
+
+    String getString(String path, String def);
+
+    long getLong(String path);
+
+    long getLong(String path, long def);
+
+    int getInt(String path);
+
+    int getInt(String path, int def);
+
+    boolean getBoolean(String path);
+
+    boolean getBoolean(String path, boolean def);
+
+    Collection<String> getKeys(String path);
 }
