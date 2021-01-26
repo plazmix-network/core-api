2e55a4db90bee7ad33ac4b4791bc39c6f90f6674
MasterCapeXD <mastercapexd@gmail.com>
2021-01-26 13:52:24 +0200

    Make DatabaseService instance of Service

--- src/main/java/net/plazmix/core/api/service/database/JdbcDatabaseService.java
+++ src/main/java/net/plazmix/core/api/service/database/JdbcDatabaseService.java
@@ -1,10 +1,12 @@
 package net.plazmix.core.api.service.database;
 
+import net.plazmix.core.api.service.Service;
+
 import javax.sql.DataSource;
 import java.io.File;
 import java.util.Optional;
 
-public interface JdbcDatabaseService {
+public interface JdbcDatabaseService extends Service {
 
     JdbcDataSourceConfig configureFromFile(File file);
 
