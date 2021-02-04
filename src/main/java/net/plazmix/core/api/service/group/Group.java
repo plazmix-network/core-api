7b2bfb4d89ba03982c9b369282f0d6a1fc4a8bec
MasterCapeXD <mastercapexd@gmail.com>
2021-02-04 19:52:26 +0200

    Update Group values

--- src/main/java/net/plazmix/core/api/service/group/Group.java
+++ src/main/java/net/plazmix/core/api/service/group/Group.java
@@ -12,7 +12,15 @@ public enum Group {
     COMET("&d", "", "&d&lComet", false),
     STAR("&6", "", "&6&lStar", false),
     GALAXY("&5", "", "&5&lGalaxy", false),
-    COSMO("&1", "", "&1&lCosmo", false);
+    COSMO("&1", "", "&1&lCosmo", false),
+
+    CHAT_MODER("&2&lCHAT MODER &2", "", "&2&lCHAT MODER", true),
+    MODER("&9&lMODER &9", "", "&9&lMODER", true),
+    TRUE_MODER("&3&lMODER+ &3", "", "&3&lMODER+", true),
+    MANAGER("&9&lMANAGER &9", "", "&9&lMANAGER", true),
+    CURATOR("&c&lCURATOR &c", "", "&c&lCURATOR", true),
+    DEV("&9&lDEV &9", "", "&9&lDEV", true),
+    ADMIN("&4&lADMIN &4", "", "&4&lADMIN", true);
 
     private final String prefix, suffix, displayName;
     private final boolean staff;
